a32d8fd423a35bd61893ef2d69cf99f1a8ce4c1e
Brian Franklin <brianfranklinmusic@gmail.com>
2020-11-14 17:30:38 -0500

    added while loop to info option

--- lib/a24/cli.rb
+++ lib/a24/cli.rb
@@ -1,11 +1,11 @@
 class A24::CLI
   
   def call
+    system("clear")
     menu
   end
 
   def menu
-    system("clear") 
     puts "Welcome to the A24 Movies App! What would you like to do? "
 
     input = nil
@@ -19,26 +19,43 @@ class A24::CLI
       input = gets.strip.downcase
       case input
       when "list"
-        A24::Film.all.each.with_index(1) do |film, i|
-          puts "#{i}. #{film.title}"
-        end
+        list
       when "recommend"
-        sample = A24::Film.all.sample
-        A24::Film.get_info_or_display_info(sample)
+        recommend
       when "info"
-        puts "Enter the name of the film you want more info on: "
-        name = gets.strip.downcase
-        A24::Film.all.each do |film|
-          if film.title.downcase == name
-            A24::Film.get_info_or_display_info(film)
-          end
-        end
+        info
       when "exit"
         puts "Have a great day!!"
       else
         puts "I didn't understand your request."
       end
     end
+  end
+
+  def list
+    puts "------------------------------------------------------------"
+    A24::Film.all.each.with_index(1) do |film, i|
+      puts "#{i}. #{film.title}"
+    end
+    puts "------------------------------------------------------------"
+  end
+
+  def recommend
+    sample = A24::Film.all.sample
+    A24::Film.get_info_or_display_info(sample)
+  end
+
+  def info
+    name = nil
+      while !A24::Film.all.detect {|film| film.title.downcase == name} && name != "exit"
+        puts "Enter the name of the film you want more info on or type exit: "
+        name = gets.strip.downcase
+        A24::Film.all.each do |film|
+          if film.title.downcase == name
+            A24::Film.get_info_or_display_info(film)
+          end
+        end
+      end
   end
 
 end
